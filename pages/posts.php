c31a0637253657bdd317b42e34a34333a1cc0d33
Lorran <lorrangtech@gmail.com>
2025-07-09 18:50:14 -0300

    new posts page

--- pages/posts.php
+++ pages/posts.php
@@ -7,23 +7,23 @@
   <title>Works</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../assets/css/header.css">
-  <link rel="stylesheet" href="../assets/css/works.css">
+  <link rel="stylesheet" href="../assets/css/posts.css">
 </head>
 <body>
   <div class="container py-5 fade-in section-wrapper">
 
-    <h2 class="section-title mb-4">Works</h2>
+    <h2 class="section-title mb-4">Posts</h2>
     <div class="project-grid mb-5">
       <div class="project-card fade-in">
-        <img src="/assets/images/image2.webp" alt="Projeto 1" class="project-thumb">
-        <h5 class="project-title">Projeto 1</h5>
-        <p class="project-desc">Lorem ipsum dolor sit amet</p>
+        <a href="https://medium.com/@lorranprt/value-those-who-take-action-not-just-talk-c0d75657db9e"><img src="/assets/images/post2.png" alt="Post1" class="post-thumb"></a>
+        <h5 class="project-title">Value Those Who Take Action, Not Just Talk</h5>
+        <p class="project-desc">Talk is easy — action is what matters. True friendships are proven through actions, not promises. Surround yourself with doers, not just talkers — and be that person too.</p>
       </div>
 
       <div class="project-card fade-in">
-        <img src="/assets/images/image2.webp" alt="Projeto 2" class="project-thumb">
-        <h5 class="project-title">Projeto 2</h5>
-        <p class="project-desc">Lorem ipsum dolor sit amet</p>
+        <a href="https://medium.com/@lorranprt/are-we-using-artificial-intelligence-the-right-way-9c583fd86f90"><img src="/assets/images/post1.png" alt="Post2" class="post-thumb"></a>
+        <h5 class="project-title">Are We Using Artificial Intelligence the Right Way?</h5>
+        <p class="project-desc">AI is powerful — but its true value depends on how we use it: responsibly, ethically, and for the good of all.</p>
       </div>
     </div>
     </div>
